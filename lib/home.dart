ae515b220b2442ccd1d24120d666c3dc1c208cb6
Eslamghazi1 <eslamghazi0@gmail.com>
2024-09-23 00:32:20 +0300

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,5 @@
 import 'controller_main.dart';
-import 'details.dart';
+import 'Image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:transparent_image/transparent_image.dart';
