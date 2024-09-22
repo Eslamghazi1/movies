28b08a798d7e07c26dd1202d32292dd5c27424f9
Eslamghazi1 <eslamghazi0@gmail.com>
2024-09-23 00:31:08 +0300

    Update Image_picker.dart

--- lib/Image_picker.dart
+++ lib/Image_picker.dart
@@ -1,3 +1,4 @@
+import 'test.dart';
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
 
@@ -16,34 +17,66 @@ body: SafeArea(
       Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          FadeInImage(
-              fit: BoxFit.fitHeight,
-              height: MediaQuery.of(context).orientation==Orientation.portrait?
-              MediaQuery.of(context).size.height/4:
-              MediaQuery.of(context).size.height/3,
-              width: MediaQuery.of(context).orientation==Orientation.portrait?
-              MediaQuery.of(context).size.width/3:
-              MediaQuery.of(context).size.height/4,
-              placeholder: MemoryImage(kTransparentImage),
-              image:NetworkImage(
-                  details["poster_path"]==null?
-                  details["backdrop_path"]==null?
-                  "https://w0.peakpx.com/wallpaper/20/806/HD-wallpaper-friends-animated-tv-series.jpg"
-                      :
-                  "https://image.tmdb.org/t/p/original${details["backdrop_path"]}"
-                      : "https://image.tmdb.org/t/p/original${details["poster_path"]}"
-              )),
+          InkWell(
+            splashColor: null,
+            onTap: (){
+              showDialog(context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog( backgroundColor: Colors.white.withOpacity(0.0),
+                    content:  FadeInImage(
+                        fit: BoxFit.fitHeight,
+                        height: MediaQuery.of(context).orientation==Orientation.portrait?
+                        MediaQuery.of(context).size.height/2:
+                        MediaQuery.of(context).size.height/2,
+                        width: MediaQuery.of(context).orientation==Orientation.portrait?
+                        MediaQuery.of(context).size.width/2:
+                        MediaQuery.of(context).size.height/2,
+                        placeholder: MemoryImage(kTransparentImage),
+                        image:NetworkImage(
+                            details["poster_path"]==null?
+                            details["backdrop_path"]==null?
+                            "https://w0.peakpx.com/wallpaper/20/806/HD-wallpaper-friends-animated-tv-series.jpg"
+                                :
+                            "https://image.tmdb.org/t/p/original${details["backdrop_path"]}"
+                                : "https://image.tmdb.org/t/p/original${details["poster_path"]}"
+                        )),);},
+              );
+            },
+            child: FadeInImage(
+                fit: BoxFit.fitHeight,
+                height: MediaQuery.of(context).orientation==Orientation.portrait?
+                MediaQuery.of(context).size.height/4:
+                MediaQuery.of(context).size.height/3,
+                width: MediaQuery.of(context).orientation==Orientation.portrait?
+                MediaQuery.of(context).size.width/3:
+                MediaQuery.of(context).size.height/4,
+                placeholder: MemoryImage(kTransparentImage),
+                image:NetworkImage(
+                    details["poster_path"]==null?
+                    details["backdrop_path"]==null?
+                    "https://w0.peakpx.com/wallpaper/20/806/HD-wallpaper-friends-animated-tv-series.jpg"
+                        :
+                    "https://image.tmdb.org/t/p/original${details["backdrop_path"]}"
+                        : "https://image.tmdb.org/t/p/original${details["poster_path"]}"
+                )),
+          ),
 
           Expanded(
             child: Column(
               children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Ratewidget(rate:details["vote_average"]),
+                  ],
+                ),
                 Text( details["name"]==null? details["title"]
                               :details["name"] ,
                       textAlign: TextAlign.center,style: TextStyle(fontSize: 20),
                   overflow: TextOverflow.fade,
                 ),
                 SizedBox(width: 3,height: 3,),
-                Text("Contry: ${details["origin_country"].toString()}",
+                Text("Country: ${details["origin_country"].toString()}",
                   style: TextStyle(fontSize: 20),
                   overflow: TextOverflow.fade,
                 ),
@@ -67,9 +100,9 @@ body: SafeArea(
         ],
       ),
       SizedBox(width: 10,height: 10,),
-      Text(" Overview",style:TextStyle(fontSize: 25,color: Theme.of(context).primaryColor) ),
+      Text(" Overview",style:TextStyle(fontSize: 30,color: Theme.of(context).primaryColor) ),
       Text( details["overview"],
-        textAlign: TextAlign.center,style: TextStyle(fontSize: 20),
+      style: TextStyle(fontSize: 20),
         overflow: TextOverflow.fade,
       ),
     ],
@@ -78,4 +111,3 @@ body: SafeArea(
     );
   }
 }
-
