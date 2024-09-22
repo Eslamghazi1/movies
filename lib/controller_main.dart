7463abfd8d8d4634550e8da678813463075485b9
Eslamghazi1 <eslamghazi0@gmail.com>
2024-09-22 16:41:19 +0300

    Update controller_main.dart

--- lib/controller_main.dart
+++ lib/controller_main.dart
@@ -8,19 +8,43 @@ abstract class MainController extends GetxController{
 }
 
 class MainControllerIMP extends MainController{
-  Map  trendingresults= {};
-
+  Map  toptv= {};
+  Map  puplartv={};
+  Map  puplarmovies={};
+  Map  topmovies={};
+  Map  anothermoviestop={};
+  Map  moviespuplar={};
+  Map another={};
+  Map searched={};
   @override
   loadmovies()async {
-    TMDB tmdb=TMDB(ApiKeys(API_KEY,API_TOKEN),
-    logConfig: ConfigLogger(showLogs: true,
-    showErrorLogs: true));
+   try {
+      TMDB tmdb = TMDB(ApiKeys(API_KEY, API_TOKEN),
+          logConfig: ConfigLogger(showLogs: true, showErrorLogs: true));
+      toptv = await tmdb.v3.tv.getTopRated();
+      puplartv= await tmdb.v3.tv.getPopular();
+      puplarmovies= await tmdb.v3.movies.getPopular();
+      topmovies = await tmdb.v3.trending.getTrending(timeWindow:TimeWindow.week);
+      anothermoviestop= await tmdb.v3.movies.getTopRated();
+      moviespuplar= await tmdb.v3.movies.getPopular();
+      // another =await tmdb.v3.collections.getDetails(5);
+      // print("the collection you have is :$another");
 
-    Map trendingresults=await tmdb.v3.tv.getPopular();
-    print("++++++++++++++++++++");
-    print(trendingresults);
-    return trendingresults;
-    update();
+
+
+
+      update();
+      return "done";
+    }catch (e){
+     return "failed";
+   }
+  }
+  searchdata(String data)async{
+    TMDB tmdb = TMDB(ApiKeys(API_KEY, API_TOKEN),
+        logConfig: ConfigLogger(showLogs: true, showErrorLogs: true));
+    searched = await tmdb.v3.search.queryMovies(data);
+    return searched;
+
   }
 
 }
