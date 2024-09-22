09e7aee193ddb0ca0b414950b14ea0f7f9b5bfb3
Eslamghazi1 <eslamghazi0@gmail.com>
2024-09-23 00:30:05 +0300

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,1 +1,46 @@
 
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'controller_main.dart';
+import 'home.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  MainControllerIMP controller = Get.put(MainControllerIMP());
+  loading()async{
+   await Future.delayed(Duration(seconds: 1));
+   Get.offAll(()=>Home());
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loading();
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+
+      child: Scaffold(
+        body: Container(
+          color: Colors.blueGrey.withAlpha(30),
+          height: double.infinity,
+          width: double.infinity,
+          child:Center(child: Image.asset("images/movies.png",
+            width: 100,
+            height: 100,
+          ),)
+        ),
+        ),
+
+    );
+  }
+}
